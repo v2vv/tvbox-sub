010cbfae7d1723664d6ba3228f982c96f3dd803c
v2vv <v2vvcn@gmail.com>
2024-11-16 13:48:51 +0000

    添加 _routes.json

--- functions/ctrl.js
+++ functions/ctrl.js
@@ -1,5 +1,7 @@
 export async function onRequest(context) {
     await context.env.TVBOX.put("first-key", "This is the value for the key");
-    const first_key = await context.env.TVBOX.get("first-key");
+    const first_key = await context.env.TVBOX.get("first-key"); 
     return new Response(`Hello, world ${first_key}!`, { status: 200 });
 }
+
+
