842fff30f207562469d7b467af5fecaec9f5a6d2
v2vv <v2vvcn@gmail.com>
2024-11-16 12:44:04 +0000

    test2

--- functions/ctrl.js
+++ functions/ctrl.js
@@ -1,4 +1,3 @@
 export async function onRequest(request, env, ctx) {
-    await env.TVBOX.put("first-key", "This is the value for the key");
     return new Response('Hello, world!', { status: 200 });
 }
