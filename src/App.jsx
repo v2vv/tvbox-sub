057427cbe834c5b033a52fcfdaa984b1e0289c08
v2vv <v2vvcn@gmail.com>
2024-11-17 02:23:51 +0000

    减少导航与顶部宽度

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navigation from './components/Navigation'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-gray-50 py-2">
       <Navigation />
     </div>
   )
