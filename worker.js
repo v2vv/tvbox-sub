ef8676bc1845af0a87ddec2710dd3b70318350d6
v2vv <v2vvcn@gmail.com>
2024-11-29 07:08:02 +0000

    优化worker代码

--- worker.js
+++ worker.js
@@ -19,10 +19,31 @@ export default {
       return new Response(null, { headers: corsHeaders });
     }
 
+    // 帮助函数：转义正则中的特殊字符
+    const escapeRegExp = (str) =>
+      str.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&");
+
     // 通用的缓存和响应头设置
     const addCustomHeaders = (response) => {
       response.headers.set("x-foo", "bar");
       return response;
+    };
+
+    // 获取替换后的字符串
+    const replaceWithTokens = (data_string) => {
+      return data_string
+        .replace(
+          new RegExp(escapeRegExp("file://TV/ali_token.txt"), "g"),
+          ali_token
+        )
+        .replace(
+          new RegExp(escapeRegExp("file://TV/quark_cookie.txt"), "g"),
+          quark_cookie
+        )
+        .replace(
+          new RegExp(escapeRegExp("file://TV/uc_cookie.txt"), "g"),
+          uc_cookie
+        );
     };
 
     // 路径处理映射
@@ -36,85 +57,31 @@ export default {
           `https://py.nxog.top/zm/api/jm/api.php?ou=${encodeURIComponent(
             "http://ok321.top/tv"
           )}`,
-          {
-            method: "GET",
-          }
+          { method: "GET" }
         );
 
-        // 检查响应状态是否为 200
         if (!response.ok) {
           throw new Error("网络响应失败");
         }
 
-        // 获取响应的 JSON 数据
         const data = await response.json();
-        const data_string = JSON.stringify(data);
-        // 帮助函数：转义正则中的特殊字符
-        const escapeRegExp = (str) =>
-          str.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&");
+        const customDataString = replaceWithTokens(JSON.stringify(data));
 
-        const custom_data_string = data_string
-          .replace(
-            new RegExp(escapeRegExp("file://TV/ali_token.txt"), "g"),
-            ali_token
-          )
-          .replace(
-            new RegExp(escapeRegExp("file://TV/quark_cookie.txt"), "g"),
-            quark_cookie
-          )
-          .replace(
-            new RegExp(escapeRegExp("file://TV/uc_cookie.txt"), "g"),
-            uc_cookie
-          );
-
-        return new Response(custom_data_string, {
-          headers: {
-            ...corsHeaders,
-            "Content-Type": "application/json",
-          },
+        return new Response(customDataString, {
+          headers: { ...corsHeaders, "Content-Type": "application/json" },
         });
       },
       "/sub/ouge": async () => {
-        console.log("start");
         const response = await fetch(
           "https://ua.fongmi.eu.org/box.php?url=http%3A%2F%2Ftv.nxog.top%2Fm%2F",
-          {
-            method: "GET",
-          }
+          { method: "GET" }
         );
-        // 获取响应的 JSON 数据
-        const data_string = await response.text();
 
-        // const data_string = JSON.stringify(data);
+        const dataString = await response.text();
+        const customDataString = replaceWithTokens(dataString);
 
-        // 帮助函数：转义正则中的特殊字符
-        const escapeRegExp = (str) =>
-          str.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&");
-
-        const custom_data_string = data_string
-          .replace(
-            new RegExp(escapeRegExp("http://127.0.0.1:9978/file/TV/.uc"), "g"),
-            uc_cookie
-          )
-          .replace(
-            new RegExp(
-              escapeRegExp("http://127.0.0.1:9978/file/TV/.quark"),
-              "g"
-            ),
-            quark_cookie
-          )
-          .replace(
-            new RegExp(
-              escapeRegExp("http://127.0.0.1:9978/file/TV/token.txt"),
-              "g"
-            ),
-            ali_token
-          );
-        return new Response(custom_data_string, {
-          headers: {
-            ...corsHeaders,
-            "Content-Type": "application/json",
-          },
+        return new Response(customDataString, {
+          headers: { ...corsHeaders, "Content-Type": "application/json" },
         });
       },
     };
@@ -125,7 +92,6 @@ export default {
     if (pathHandlers[pathname]) {
       res = await pathHandlers[pathname](request);
     } else {
-      // 对于未知路径，返回 404
       res = new Response("404 Not Found", {
         status: 404,
         headers: corsHeaders,
